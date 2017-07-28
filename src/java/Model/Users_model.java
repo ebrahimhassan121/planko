1c13c6506de8ce388fe5b2cb6d099356c816dfb9
ebrahimhassan121 <ebrahim.has11@ci.menofia.edu.eg>
2017-07-28 05:27:53 +0200

    Register ADDED

--- src/java/Model/Users_model.java
+++ src/java/Model/Users_model.java
@@ -7,6 +7,7 @@ package Model;
 
 import beans.user_bean;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -20,14 +21,14 @@ import java.util.List;
 public class Users_model {
 
     CheckDatabase cd = new CheckDatabase();
-    
+
     Connection connect = null;
     ResultSet re = null;
 
     public user_bean select_user(String email, String password) throws SQLException {
         user_bean user = new user_bean();
         ResultSet rs = null;
-       String UserDetails_ID = null;
+        String UserDetails_ID = null;
         try {
             connect = cd.check();
             String query = " Select `UserID`, `UserEmail`, `Password`, `role_id`, `phone`, `Facebook`, `twitter`, `Active` From users WHERE `UserEmail`='" + email + "' AND `password`='" + password + "' ";
@@ -41,21 +42,16 @@ public class Users_model {
                 return null;
             }
             while (rs.next()) {
-                user.setUserID(UserDetails_ID=rs.getString("UserID"));
+                user.setUserID(UserDetails_ID = rs.getString("UserID"));
                 user.setPassword(rs.getString("password"));
                 user.setEmail(rs.getString("UserEmail"));
-                user.setRole(rs.getString("role_id"));
+                user.setRole(rs.getInt("role_id")+"");
                 user.setFacebook(rs.getString("Facebook"));
                 user.setTwitter(rs.getString("twitter"));
                 user.setPhone(rs.getString("phone"));
 
             }
-//            query="SELECT `UserDetails_ID`, `Name`, `Address`, `about` FROM `user_details` WHERE `UserDetails_ID` ="+UserDetails_ID+"";
-//            rs=ps.executeQuery(query);
-//            while(rs.next()){
-//               user.setName(rs.getString("Name"));
-//               user.setAddress("");
-//            }
+
         } catch (SQLException e) {
             System.out.println("error/n" + e.getMessage());
         } finally {
@@ -65,5 +61,37 @@ public class Users_model {
         }
         return user;
     }
-
+    public boolean CheckEmail(String email) throws SQLException{
+            connect = cd.check();
+            ResultSet rs;
+            String query = " Select  `UserEmail`From users WHERE `UserEmail`='" + email + "' ";
+            Statement ps = connect.createStatement();
+            rs = ps.executeQuery(query);
+            return (rs.next());
+    }
+    public boolean insert_user(user_bean user,int Role) {
+        ResultSet rs = null;
+        String UserDetails_ID = null;
+          boolean isInserted=false;
+        try {
+            connect = cd.check();
+            //INSERT INTO `users`(`UserID`, `UserEmail`, `Password`, `role_id`, `phone`, `Facebook`, `twitter`, `Active`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])
+            String query = "INSERT INTO `users`(`UserID`, `UserEmail`, `Password`, `role_id`, `phone`, `Facebook`, `twitter`, `Active`) VALUES (?,?,?,?,?,?,?,?)";
+            PreparedStatement preparedStatement = connect.prepareStatement(query);
+            preparedStatement.setInt(1,0);
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.setInt(4, Role);
+            preparedStatement.setString(5,"");
+            preparedStatement.setString(6,"");
+            preparedStatement.setString(7,"");
+            preparedStatement.setInt(8,1);
+            preparedStatement.execute();
+            isInserted=true;
+          
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+          return isInserted;
+    }
 }
